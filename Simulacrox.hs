61b39644f9495912a9e4f5bf6bf6b953cc4e60e3
Reginaldo1107 <reginaldohinojosabaldera@gmail.com>
2023-09-18 20:45:37 -0300

    agregoEjercicio1  Simulacrox.hs

--- Simulacrox.hs
+++ Simulacrox.hs
@@ -43,3 +43,67 @@ incluido x (y:ys)| x==y = True
 --asegura: {res tiene exactamente los elementos que figuran en las tuplas de relaciones en las que una de sus componentes es persona}
 
 
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--problema relacionesValidas (relaciones: seq⟨String x String⟩) : Bool { 
+--requiere: {True}
+--asegura: {(res = true) <=> relaciones no contiene ni tuplas repetidas1, ni tuplas con ambas componentes iguales}
+-- *NI TUPLAS CON AMBAS COMPONENTES IGUALES
+
+-- [("123","123")] ---> False  , YALA 
+
+-- *NI TUPLAS REPETIDAS 
+-- [("123","456") , ("123","456") ]  --> False 
+
+--tuplasIguales :: [(String ,String)] ->Bool
+--tuplasIguales ((x,y): (xs))
+
+
+relacionesValidas :: [(String ,String)] -> Bool
+relacionesValidas [] = True
+relacionesValidas ((a,b) :xs)   | (a == b) = False 
+                                | perteneceGeneral (a,b) xs = False 
+                                | perteneceGeneral (b,a) xs = False
+                                |otherwise = relacionesValidas xs
+
+-- pertenece a la lista osea no son iguales
+perteneceGeneral :: (Eq t) => t -> [t] ->Bool
+perteneceGeneral x [] = False
+perteneceGeneral x (y:ys)   | x == y = True 
+                            |otherwise = perteneceGeneral x (ys)
+
+-- *En iguales hayn 3 casos : 
+-- *Cuando esta vacia la lista 
+-- *Cuando solo hay un elemento 
+-- *Cuando la lista esta llena 
+perteneceInt :: Integer -> [Integer] -> Bool
+perteneceInt x  []  = False -- Cuando la lista es vacia 
+
+perteneceInt x (y:[]) | x == y  = True 
+                 |otherwise = False
+
+perteneceInt x (y:xs) | x == y  = True 
+                 |otherwise = perteneceInt x (xs)
+
+
+
+
+elementosEnComunLista :: [Integer] -> Bool
+elementosEnComunLista [] = False
+elementosEnComunLista (x:[]) = False
+elementosEnComunLista (x:xs)  |perteneceGeneral x (xs) = True
+                               |otherwise = elementosEnComunLista(xs)
+    
+
+--problema personas (relaciones: seq⟨String x String⟩) : seq⟨String⟩ {
+--requiere: {relacionesValidas(relaciones)}
+-- asegura: {resu tiene exactamente los elementos que figuran en alguna 
+--tupla de relaciones en cualquiera de las dos
+--posiciones, sin repetir}
+--}
+personas :: [(String ,String)] -> [String]
+
+
+--Tengo una lista repetida tengo que devolver una lista sin repetir de enteros 
+listaDeEnteroSinRepetir :: [Integer]-> [Integer]
+ 
+
